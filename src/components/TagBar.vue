25eb340f1c7fd62e2694a205f9551235cb7b143e
davidmichaelm <davemarquardt@gmail.com>
2020-11-16 10:01:00 -0600

    Ability to set tag color

--- src/components/TagBar.vue
+++ src/components/TagBar.vue
@@ -24,10 +24,19 @@
         triggers="focus"
         placement="bottom">
       <div class="d-flex flex-row">
-        <input type="text" ref="tagInput" v-model="newTag" @keypress="handleKeypress">
+        <input type="text" :class="newTagColor + ' text-light'" ref="tagInput" v-model="newTag" @keypress="handleKeypress">
         <a href="#" @click="addTag">
           <b-icon-plus class="h4 mb-0 text-primary"/>
         </a>
+      </div>
+      <div>
+        <a href="#" class="btn btn-primary m-1" @click="setTagColor('bg-primary')"></a>
+        <a href="#" class="btn btn-secondary m-1" @click="setTagColor('bg-secondary')"></a>
+        <a href="#" class="btn btn-success m-1" @click="setTagColor('bg-success')"></a>
+        <a href="#" class="btn btn-danger m-1" @click="setTagColor('bg-danger')"></a>
+        <a href="#" class="btn btn-warning m-1" @click="setTagColor('bg-warning')"></a>
+        <a href="#" class="btn btn-info m-1" @click="setTagColor('bg-info')"></a>
+        <a href="#" class="btn btn-dark m-1" @click="setTagColor('bg-dark')"></a>
       </div>
     </b-popover>
   </div>
@@ -48,6 +57,7 @@ export default {
   data() {
     return {
       newTag: "",
+      newTagColor: "bg-primary",
       allTagData: store.boards["0"].tags,
       popoverShow: false,
       id: Math.random()
@@ -55,6 +65,9 @@ export default {
   },
   methods: {
     onPopoverShown() {
+      this.focusPopoverInput();
+    },
+    focusPopoverInput() {
       this.$refs.tagInput.focus();
     },
     handleKeypress(event) {
@@ -70,7 +83,7 @@ export default {
 
       if (!(this.newTag in this.allTagData)) {
         this.allTagData[this.newTag] = {
-          backgroundColor: "bg-primary",
+          backgroundColor: this.newTagColor,
           color: "text-white"
         }
       }
@@ -81,6 +94,10 @@ export default {
     closePopover() {
       this.newTag = "";
       this.popoverShow = false;
+    },
+    setTagColor(color) {
+      this.newTagColor = color;
+      this.focusPopoverInput();
     }
   }
 }
