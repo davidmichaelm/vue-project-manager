db7f0cac3e71fb284ad2291350287c2c994a1a61
davidmichaelm <davemarquardt@gmail.com>
2020-11-19 15:35:00 -0600

    Add vue-debounce dependency

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
+import './plugins/vue-debounce'
 import App from './App.vue'
 
 Vue.config.productionTip = false
